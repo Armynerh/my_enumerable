33be6d9ce7485f953710029aa458ec35be142a71
Kehinde Aminat <aimenah@outlook.com>
2023-10-07 00:41:45 +0100

    Update my_list.rb with changes

--- my_list.rb
+++ my_list.rb
@@ -1,10 +1,13 @@
 require_relative 'my_enumerable'
+
 class MyList
-    include MyEnumerable
-    def initialize(list)
-      @list = list
-    end
-    def each
-        @list.each { |item| yield(item) }
-      end
+  include MyEnumerable
+
+  def initialize(list)
+    @list = list
+  end
+
+  def each
+    @list.each { |item| yield(item) }
+  end
 end
